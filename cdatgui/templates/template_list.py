d59a0bdd14d9ea5f9f8e7996ebbd9244943d9e71
Sam Fries <samuelbfries@gmail.com>
2015-08-06 07:33:43 -0700

    Made template list do its job

--- cdatgui/templates/template_list.py
+++ cdatgui/templates/template_list.py
@@ -1,5 +1,16 @@
 from PySide import QtGui
+import vcs
 
 
 class TemplateList(QtGui.QListWidget):
-    pass
+    def __init__(self, parent=None):
+        super(TemplateList, self).__init__(parent=parent)
+        self.templates = vcs.elements["template"].values()
+
+        self.addItems([tmpl.name for tmpl in self.templates])
+
+    def get_selected(self):
+        ind = self.currentRow()
+        if ind == -1:
+            return None
+        return self.templates[ind]
