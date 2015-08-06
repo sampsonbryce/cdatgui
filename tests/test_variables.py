5030998694c50d5f912a335db9ac211641ebdc23
Sam Fries <samuelbfries@gmail.com>
2015-08-06 11:58:53 -0700

    Manager is at 100% coverage

--- tests/test_variables.py
+++ tests/test_variables.py
@@ -12,28 +12,28 @@ def test_cdms_file_tree_add_file(qtbot):
 
     tree.add_file(cdms2.open(sample_file))
 
-    # Make sure the item was added
+    # the item was added
     assert tree.topLevelItemCount() == 1
 
     tli = tree.topLevelItem(0)
-    # Make sure it grabbed the filename correctly
+    # it grabbed the filename correctly
     assert tli.text(1) == "clt.nc"
 
-    # Make sure new files are expanded
+    # new files are expanded
     assert tli.isExpanded()
 
-    # Make sure that it loaded the variables correctly
+    # that it loaded the variables correctly
     assert tli.childCount() == 3
 
     file_variables = ["clt", "u", "v"]
 
     for i in range(tli.childCount()):
         child = tli.child(i)
-        # Make sure that it placed the variables in
+        # that it placed the variables in
         # the order they were in the file
         assert file_variables.index(child.text(1)) == i
 
-    # Make sure we can't add the same file twice
+    # we can't add the same file twice
     with pytest.raises(ValueError):
         tree.add_file(cdms2.open(sample_file))
 
@@ -55,7 +55,7 @@ def test_cdms_file_tree_get_selected(qtbot):
 
     selected_vars = tree.get_selected()
 
-    # Make sure it returned the clt variable
+    # it returned the clt variable
     assert selected_vars[0].id == "clt"
 
     u = tli.child(1)
@@ -74,12 +74,12 @@ def test_cdms_var_list_add_var(qtbot):
 
     varlist.add_variable(clt("clt"))
 
-    # Make sure it has an item in the list
+    # it has an item in the list
     assert varlist.count() == 1
 
     item = varlist.item(0)
 
-    # Make sure it has the variable name as the text
+    # it has the variable name as the text
     assert item.text() == "clt"
 
 
@@ -89,7 +89,7 @@ def test_cdms_var_list_get_var(qtbot):
     clt = cdms2.open(vcs.sample_data + "/clt.nc")
     varlist.add_variable(clt("clt"))
 
-    # Make sure it's the same variable
+    # it's the same variable
     var = varlist.get_variable(0)
     assert var.id == "clt"
 
@@ -97,7 +97,7 @@ def test_cdms_var_list_get_var(qtbot):
 def test_cdms_file_chooser_browse(qtbot):
     chooser = cdatgui.variables.cdms_file_chooser.CDMSFileChooser()
 
-    # Make sure it's on the file browser
+    # it's on the file browser
     chooser.tabs.set_current_row(0)
 
     qtbot.addWidget(chooser)
@@ -121,3 +121,19 @@ def test_cdms_file_chooser_browse(qtbot):
     l.setCurrentItem(None)
 
     assert chooser.accepted_button.isEnabled() is False
+
+
+def test_manager():
+    man = cdatgui.variables.manager.Manager()
+    clt = man.get_file(vcs.sample_data + "/clt.nc")
+
+    # The file is the same as the one requested
+    assert clt.id == vcs.sample_data + "/clt.nc"
+    # Retrieving the same file returns the exact same object
+    assert id(clt) == id(man.get_file(vcs.sample_data + "/clt.nc"))
+    # Retrieving a nonexistant object gives an IOError
+    with pytest.raises(IOError):
+        man.get_file("/tmp/filedoesnotexist")
+    # Will only retrieve correct file types
+    with pytest.raises(IOError):
+        man.get_file(__file__)
