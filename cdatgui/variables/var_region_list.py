fe91fb81e5bb29069edfca49bfd99c96cdb03e3f
Samuel Fries <fries2@llnl.gov>
2015-10-02 08:49:46 -0700

    Integrated ROIPreview into QAxisList for live preview of ROI

--- cdatgui/variables/var_region_list.py
+++ cdatgui/variables/var_region_list.py
@@ -12,15 +12,20 @@ def continents():
     return __continents__
 
 
-def continents_in_latlon(lat_range, lon_range):
+def continents_in_latlon(lat_range, lon_range, size=(200, 200)):
     c = continents()
     low_x, high_x = lon_range
     low_y, high_y = lat_range
 
+    # Adjust into first quadrant
     low_x += 180
     high_x += 180
     low_y += 90
     high_y += 90
+
+    # Adjust into 4th quadrant
+    low_y = 180 - low_y
+    high_y = 180 - high_y
 
     low_x_pct = low_x / 360.
     high_x_pct = high_x / 360.
@@ -29,23 +34,42 @@ def continents_in_latlon(lat_range, lon_range):
 
     image_x1 = low_x_pct * c.width()
     image_x2 = high_x_pct * c.width()
-    image_y1 = low_y_pct * c.height()
-    image_y2 = high_y_pct * c.height()
+    # The "high" y is closer to 0, which means it's the start corner
+    image_y1 = high_y_pct * c.height()
+    # The "low" y is further from 0, which means it's the end corner
+    image_y2 = low_y_pct * c.height()
 
     cropped = c.copy(image_x1, image_y1, image_x2 - image_x1, image_y2 - image_y1)
-
+    if 0 in (cropped.width(), cropped.height()):
+        return QtGui.QPixmap.fromImage(cropped)
     if cropped.height() > cropped.width():
-        scaled = cropped.scaledToHeight(200, QtCore.Qt.SmoothTransformation)
+        scaled = cropped.scaledToHeight(size[1], QtCore.Qt.SmoothTransformation)
     else:
-        scaled = cropped.scaledToWidth(200, QtCore.Qt.SmoothTransformation)
+        scaled = cropped.scaledToWidth(size[0], QtCore.Qt.SmoothTransformation)
 
     return QtGui.QPixmap.fromImage(scaled)
 
 
 class ROIPreview(QtGui.QLabel):
-    def __init__(self, lat_range=(-90, 90), lon_range=(-180, 180), parent=None):
+    def __init__(self, size, lat_range=(-90, 90), lon_range=(-180, 180), parent=None):
         super(ROIPreview, self).__init__(parent)
+        width, height = size
+
+        self.setMinimumWidth(width)
+        self.setMaximumWidth(width)
+        self.setMinimumHeight(height)
+        self.setMaximumHeight(height)
+        self.size = size
+
         self.lat_range = lat_range
         self.lon_range = lon_range
         self.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
-        self.setPixmap(continents_in_latlon(self.lat_range, self.lon_range))
+        self.setPixmap(continents_in_latlon(self.lat_range, self.lon_range, size=size))
+
+    def setLatRange(self, low, high):
+        self.lat_range = (low, high)
+        self.setPixmap(continents_in_latlon(self.lat_range, self.lon_range, size=self.size))
+
+    def setLonRange(self, low, high):
+        self.lon_range = (low, high)
+        self.setPixmap(continents_in_latlon(self.lat_range, self.lon_range, size=self.size))
