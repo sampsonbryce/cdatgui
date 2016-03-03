b5ede9a96456e12d6f2d0468b0b8937db8bc99f9
Sam Fries <fries2@llnl.gov>
2016-03-03 13:04:28 -0800

    Fixes bug with deleting preview widgets

--- cdatgui/cdat/vcswidget.py
+++ cdatgui/cdat/vcswidget.py
@@ -92,7 +92,8 @@ class QVCSWidget(QtGui.QFrame):
         deallocating. Overriding PyQt deleteLater to free up
         resources
         """
-        self.canvas.onClosing((0, 0))
+        if self.canvas:
+            self.canvas.onClosing((0, 0))
         self.canvas = None
         super(QVCSWidget, self).deleteLater()
 
