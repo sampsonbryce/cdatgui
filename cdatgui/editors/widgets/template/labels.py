ffc2dd4e35d10e93781a1c37863880e1402186cc
Sam Fries <fries2@llnl.gov>
2016-03-09 13:47:19 -0800

    Implemented basic template editor for labels

--- cdatgui/editors/widgets/template/labels.py
+++ cdatgui/editors/widgets/template/labels.py
@@ -2,6 +2,13 @@ from PySide import QtGui, QtCore
 import vcs
 from cdatgui.utils import icon
 
+
+members = ["file", "function", "logicalmask", "transformation", "source", "dataname",
+		   "title", "units", "crdate", "crtime", "comment1", "comment2", "comment3",
+		   "comment4", "xname", "yname", "zname", "tname", "xunits", "yunits", "zunits",
+		   "tunits", "xvalue", "yvalue", "zvalue", "tvalue", "mean", "min", "max"]
+
+
 active, inactive, toggle_icon = None, None, None
 
 
@@ -24,35 +31,21 @@ class TemplateLabelWidget(QtGui.QWidget):
 	def __init__(self, label, parent=None):
 		super(TemplateLabelWidget, self).__init__(parent=parent)
 		initmod()
-		self.member = label
+		self.member = None
 		layout = QtGui.QHBoxLayout()
 
-		self.label = QtGui.QLabel(label.member)
+		self.label = QtGui.QLabel(label)
 		self.style_picker = QtGui.QComboBox()
 		
-		cur_val = "default"
 		els = [tt for tt in vcs.elements["texttable"].keys() if tt in vcs.elements["textorientation"]]
 		for t in els:
-			if t == self.member.texttable and t == self.member.textorientation:
-				cur_val = t
 			self.style_picker.addItem(t)
-		
-		ind = els.index(cur_val)
 
-		self.style_picker.setCurrentIndex(ind)
 		self.edit_style = QtGui.QPushButton("Edit Style")
 
 		self.hide = QtGui.QPushButton(toggle_icon, u"")
 		self.hide.setStyleSheet("QPushButton{border: none; outline: none;}")
 		self.hide.setCheckable(True)
-
-		if self.member.priority == 0:
-			self.hide.setToolTip("Show Label")
-			self.hide.setChecked(False)
-		else:
-			self.hide.setToolTip("Hide Label")
-			self.hide.setChecked(True)
-
 		self.hide.toggled.connect(self.toggle_vis)
 
 		self.move = QtGui.QPushButton("Move Label")
@@ -64,10 +57,24 @@ class TemplateLabelWidget(QtGui.QWidget):
 		layout.addWidget(self.move)
 		self.setLayout(layout)
 
+	def setLabel(self, label):
+		self.member = label
+		ind = self.style_picker.findText(label.texttable)
+		if ind != -1:
+			self.style_picker.setCurrentIndex(ind)
+		if self.member.priority == 0:
+			self.hide.setToolTip("Show Label")
+			self.hide.setChecked(False)
+		else:
+			self.hide.setToolTip("Hide Label")
+			self.hide.setChecked(True)
+
 	def toggle_vis(self, show):
 		if show:
+			self.hide.setToolTip("Hide Label")
 			self.member.priority = 1
 		else:
+			self.hide.setToolTip("Show Label")
 			self.member.priority = 0
 		self.updateTemplate.emit()
 
@@ -78,6 +85,11 @@ class TemplateLabelEditor(QtGui.QWidget):
 		super(TemplateLabelEditor, self).__init__(parent=parent)
 		self._template = None
 		self._root_template = None
+		layout = QtGui.QVBoxLayout()
+		self.member_widgets = {mem: TemplateLabelWidget(mem) for mem in members}
+		for member, widget in self.member_widgets.iteritems():
+			layout.addWidget(widget)
+		self.setLayout(layout)
 
 	@property
 	def template(self):
@@ -95,13 +107,15 @@ class TemplateLabelEditor(QtGui.QWidget):
 
 	def sync(self):
 		"""Retrieve all values from the template and update the GUI."""
-		pass
+		for member in members:
+			attr = getattr(self.template, member)
+			self.member_widgets[member].setLabel(attr)
 
 if __name__ == "__main__":
 	app = QtGui.QApplication([])
 	template = vcs.createtemplate()
-	l = template.crdate
-	w = TemplateLabelWidget(l)
+	w = TemplateLabelEditor()
+	w.template = template
 	w.show()
 	w.raise_()
 	app.exec_()
